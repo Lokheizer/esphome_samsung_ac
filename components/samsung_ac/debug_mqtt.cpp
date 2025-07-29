b651ec4b3710e80a8e5b10c4829a4bb0df0455b9
Thomas <edt-tech@outlook.com>
2025-07-29 20:39:06 +0200

    Fix: does not compile on host platform
    https://github.com/omerfaruk-aran/esphome_samsung_hvac_bus/pull/263

--- components/samsung_ac/debug_mqtt.cpp
+++ components/samsung_ac/debug_mqtt.cpp
@@ -1,11 +1,10 @@
 #include "esphome/core/log.h"
 #include "debug_mqtt.h"
 
-#ifdef USE_ESP8266
+#if defined(USE_ESP8266)
 #include <AsyncMqttClient.h>
 AsyncMqttClient *mqtt_client{nullptr};
-#endif
-#ifdef USE_ESP32
+#elif defined(USE_ESP32)
 #include <mqtt_client.h>
 esp_mqtt_client_handle_t mqtt_client{nullptr};
 #endif
@@ -16,68 +15,81 @@ namespace esphome
     {
         bool debug_mqtt_connected()
         {
-#ifdef USE_ESP8266
+#if defined(USE_ESP8266) || defined(USE_ESP32)
             if (mqtt_client == nullptr)
                 return false;
 
+#if defined(USE_ESP8266)
             return mqtt_client->connected();
-#elif USE_ESP32
-            if (mqtt_client == nullptr)
-                return false;
-
+#elif defined(USE_ESP32)
             return true;
+#endif
 #else
-            return false;
+        return false;
 #endif
         }
 
         void debug_mqtt_connect(const std::string &host, const uint16_t port, const std::string &username, const std::string &password)
         {
-            if (host.length() == 0)
+            if (host.empty())
                 return;
 
-#ifdef USE_ESP8266
+#if defined(USE_ESP8266)
             if (mqtt_client == nullptr)
             {
                 mqtt_client = new AsyncMqttClient();
                 mqtt_client->setServer(host.c_str(), port);
-                if (username.length() > 0)
+                if (!username.empty())
                     mqtt_client->setCredentials(username.c_str(), password.c_str());
             }
 
             if (!mqtt_client->connected())
                 mqtt_client->connect();
-#elif USE_ESP32
+
+#elif defined(USE_ESP32)
             if (mqtt_client == nullptr)
             {
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+                // For ESP-IDF v5.0 and above
+                std::string uri = "mqtt://" + host + ":" + std::to_string(port);
+                esp_mqtt_client_config_t mqtt_cfg = {};
+                mqtt_cfg.broker.address.uri = uri.c_str();
+                if (!username.empty())
+                {
+                    mqtt_cfg.credentials.username = username.c_str();
+                    mqtt_cfg.credentials.authentication.password = password.c_str();
+                }
+#else
+                // For ESP-IDF versions below v5.0
                 esp_mqtt_client_config_t mqtt_cfg = {};
                 mqtt_cfg.host = host.c_str();
                 mqtt_cfg.port = port;
-                if (username.length() > 0)
+                if (!username.empty())
                 {
                     mqtt_cfg.username = username.c_str();
                     mqtt_cfg.password = password.c_str();
                 }
+#endif
                 mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
                 esp_mqtt_client_start(mqtt_client);
             }
+
 #endif
         }
 
         bool debug_mqtt_publish(const std::string &topic, const std::string &payload)
         {
-#ifdef USE_ESP8266
+#if defined(USE_ESP8266) || defined(USE_ESP32)
             if (mqtt_client == nullptr)
                 return false;
 
+#if defined(USE_ESP8266)
             return mqtt_client->publish(topic.c_str(), 0, false, payload.c_str()) != 0;
-#elif USE_ESP32
-            if (mqtt_client == nullptr)
-                return false;
-
+#elif defined(USE_ESP32)
             return esp_mqtt_client_publish(mqtt_client, topic.c_str(), payload.c_str(), payload.length(), 0, false) != -1;
+#endif
 #else
-            return true;
+        return false
 #endif
         }
     } // namespace samsung_ac
